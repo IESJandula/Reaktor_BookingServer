d6a1f89dc0094d1f23613d74b0ded6d952aba539
manumartin0709 <manuelmartinmurillo@gmail.com>
2025-01-29 13:06:28 +0100

    fixed project 2.0

--- src/main/java/es/iesjandula/reaktor/bookings_server/models/reservas_puntuales/Teacher.java
+++ src/main/java/es/iesjandula/reaktor/bookings_server/models/reservas_puntuales/Teacher.java
@@ -1,6 +1,7 @@
 package es.iesjandula.reaktor.bookings_server.models.reservas_puntuales;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 /**
  * teacher class
  */
+@Entity
 public class Teacher
 {
 	@Id
