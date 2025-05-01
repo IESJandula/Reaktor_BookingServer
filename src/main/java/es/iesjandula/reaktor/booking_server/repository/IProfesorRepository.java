a136885a6fa009fa6bf050de004a2338ec159fb1
MiguelDevelop <miguelriu.dev@gmail.com>
2025-05-01 15:10:25 +0200

    Fixed bugs when inserting logs as the user was not entered correctly. The log view has been modified by adding fields required for previewing logs.

--- src/main/java/es/iesjandula/reaktor/booking_server/repository/IProfesorRepository.java
+++ src/main/java/es/iesjandula/reaktor/booking_server/repository/IProfesorRepository.java
@@ -1,9 +1,13 @@
 package es.iesjandula.reaktor.booking_server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import es.iesjandula.reaktor.booking_server.models.reservas_fijas.Profesor;
 
 public interface IProfesorRepository extends JpaRepository<Profesor, String>
 {
+	@Query(value = "SELECT concat(nombre, ' ', apellidos) as Nombre FROM profesor where email = :email", nativeQuery = true)
+    String getNombreProfesor(@Param("email") String email);
 }
